9b2a18774828b127470a44b465033859b80be1fa
AbyChan <crystal.tyan@gmail.com>
2015-10-13 00:18:46 +0800

    update

---
+++ todolist.el
@@ -0,0 +1,11 @@
+
+(defvar todo-file (expand-file-name "todolist.org" user-emacs-directory))
+
+(defvar todo-file-archive (expand-file-name "todolist-archive.org" user-emacs-directory))
+
+(defun todolist ()
+  (interactive)
+  ())
+
+(provide 'todolist)
+;;; todolist.el ends here
